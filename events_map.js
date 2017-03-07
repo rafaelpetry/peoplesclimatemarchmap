bab3a8848bfc91b491329af61f990e43fa055f96
Rafael Petry <rafael@rafaelpetry.com>
2017-03-07 16:38:03 -0500

    EH/RP - Using different color markers for different types of pins.

--- events_map.js
+++ events_map.js
@@ -1,4 +1,32 @@
 PCM.EventsMap = (function() {
+
+  var blueIcon = new L.Icon({
+    iconUrl: 'leaflet/images/marker-icon-2x-blue.png',
+    shadowUrl: 'leaflet/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  var redIcon = new L.Icon({
+    iconUrl: 'leaflet/images/marker-icon-2x-red.png',
+    shadowUrl: 'leaflet/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  var greenIcon = new L.Icon({
+    iconUrl: 'leaflet/images/marker-icon-2x-green.png',
+    shadowUrl: 'leaflet/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
   function createEventsMap(mapContainer) {
     var map = L.map(mapContainer).setView([39.8282, -98.5795], 4);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -6,17 +34,17 @@ PCM.EventsMap = (function() {
     }).addTo(map);
 
     PCM.ActionNetworkGateway.fetchEvents(function(lat, lon, title) {
-      L.marker([lat, lon]).addTo(map)
+      L.marker([lat, lon], { icon: blueIcon }).addTo(map)
         .bindPopup(title);
     });
 
     PCM.GoogleSheetsGateway.fetchBuses(function(lat, lon, title) {
-      L.marker([lat, lon]).addTo(map)
+      L.marker([lat, lon], { icon: greenIcon }).addTo(map)
         .bindPopup(title);
     });
 
     PCM.GoogleSheetsGateway.fetchGroups(function(lat, lon, title) {
-      L.marker([lat, lon]).addTo(map)
+      L.marker([lat, lon], { icon: redIcon }).addTo(map)
         .bindPopup(title);
     });
   }
