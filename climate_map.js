04ce0bc2d2889a3ff0eb1a004c30fd946d567508
Rafael Petry <rafael@rafaelpetry.com>
2017-03-07 13:49:36 -0500

    EH/RP - Added pins for first page of Action Network events to map.

--- climate_map.js
+++ climate_map.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
+
+  $.ajax('https://actionnetwork.org/api/v2/event_campaigns/bd077f2f-280d-49d2-b675-e8156ee3d856/events', {
+    headers: { 'OSDI-API-Token': 'APIKEYUSEDTOGOHERE' },
+    success: function(data) {
+      var events = data['_embedded']['osdi:events'];
+      $(events).each(function(idx, event) {
+        console.log(event);
+        var lat = event['location']['location']['latitude'];
+        var lon = event['location']['location']['longitude'];
+
+        L.marker([lat, lon]).addTo(map)
+          .bindPopup(event['title']);
+      });
+    }
+  });
+
 });
