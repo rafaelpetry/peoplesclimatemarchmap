6697bbde813d327a32ac618e7381ff46f6fa36f4
Rafael Petry <rafael@rafaelpetry.com>
2017-03-07 14:18:51 -0500

    EH/RP - Added pins for additional pages of ActionNetwork events.

--- action_network_gateway.js
+++ action_network_gateway.js
@@ -1,16 +1,27 @@
 PCM.ActionNetworkGateway = (function() {
 
-  function fetchEvents(callback) {
-    $.ajax('https://actionnetwork.org/api/v2/event_campaigns/bd077f2f-280d-49d2-b675-e8156ee3d856/events', {
+  function processEvents(data, callback) {
+    var events = data['_embedded']['osdi:events'];
+    $(events).each(function(idx, event) {
+      var location = event['location']['location'];
+      callback(location['latitude'], location['longitude'], event['title']);
+    });
+  }
+
+  function fetchPage(pageUrl, callback) {
+    $.ajax(pageUrl, {
       headers: { 'OSDI-API-Token': 'APIKEYUSEDTOGOHERE' },
       success: function(data) {
-        var events = data['_embedded']['osdi:events'];
-        $(events).each(function(idx, event) {
-          var location = event['location']['location'];
-          callback(location['latitude'], location['longitude'], event['title']);
-        });
+        processEvents(data, callback);
+        try {
+          fetchPage(data['_links']['next']['href'], callback);
+        } catch(error) {};
       }
     });
+  }
+
+  function fetchEvents(callback) {
+    fetchPage('https://actionnetwork.org/api/v2/event_campaigns/bd077f2f-280d-49d2-b675-e8156ee3d856/events', callback);
   }
 
   return {
