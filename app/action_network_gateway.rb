80f5bae110b34f0f39d97aea23d1d1a3b7332f96
Rafael Petry <rafael@rafaelpetry.com>
2017-03-10 12:09:13 -0500

    EH/RP - Fetching entire list of events from Action Network.

--- app/action_network_gateway.rb
+++ app/action_network_gateway.rb
@@ -1,13 +1,27 @@
 require 'uri'
 require 'net/http'
+require 'json'
 
 class ActionNetworkGateway
-  ENDPOINT = URI('https://actionnetwork.org/api/v2/event_campaigns/bd077f2f-280d-49d2-b675-e8156ee3d856/events')
+  ENDPOINT = 'https://actionnetwork.org/api/v2/event_campaigns/bd077f2f-280d-49d2-b675-e8156ee3d856/events'
 
   def self.marches
-    request = Net::HTTP::Get.new(ENDPOINT)
+    fetch_page(ENDPOINT)
+  end
+
+  private
+
+  def self.fetch_page(page_url)
+    page_uri = URI(page_url)
+    request = Net::HTTP::Get.new(page_uri)
     request['OSDI-API-Token'] = ENV['ACTION_NETWORK_KEY']
-    response = Net::HTTP.start(ENDPOINT.hostname, ENDPOINT.port, use_ssl: true) { |http| http.request(request) }
-    JSON.parse(response.body)
+    response = Net::HTTP.start(page_uri.hostname, page_uri.port, use_ssl: true) { |http| http.request(request) }
+    body = JSON.parse(response.body)
+    events = body['_embedded']['osdi:events']
+
+    next_url = body['_links']['next']['href'] rescue nil
+    events += fetch_page(next_url) if next_url
+
+    events
   end
 end
--- spec/action_network_gateway_spec.rb
+++ spec/action_network_gateway_spec.rb
@@ -5,15 +5,46 @@ describe ActionNetworkGateway do
   describe '.marches' do
     let(:request) { {} }
     let(:http) { double }
-    let(:response) { { 'key' => 'value' } }
+    let(:event) { { 'title' => 'March!' } }
 
-    it 'fetches marches from ActionNetwork' do
-      expect(Net::HTTP::Get).to receive(:new).with(ActionNetworkGateway::ENDPOINT).and_return(request)
-      expect(Net::HTTP).to receive(:start).and_yield(http)
-      expect(http).to receive(:request).with(request).and_return(double(body: response.to_json))
+    context 'with only one page' do
+      let(:response) { { '_embedded' => { 'osdi:events' => [event] } } }
 
-      expect(ActionNetworkGateway.marches).to eq(response)
-      expect(request).to have_key('OSDI-API-Token')
+      it 'fetches marches from ActionNetwork' do
+        expect(Net::HTTP::Get).to receive(:new).with(URI(ActionNetworkGateway::ENDPOINT)).and_return(request)
+        expect(Net::HTTP).to receive(:start).and_yield(http)
+        expect(http).to receive(:request).with(request).and_return(double(body: response.to_json))
+
+        expect(ActionNetworkGateway.marches).to eq([event])
+        expect(request).to have_key('OSDI-API-Token')
+      end
+    end
+
+    context 'with multiple pages' do
+      let(:first_request) { {} }
+      let(:second_request) { {} }
+      let(:first_response) do
+        {
+          '_embedded' => { 'osdi:events' => [event] },
+          '_links' => { 'next' => { 'href' => second_page_url } }
+        }
+      end
+      let(:second_response) { { '_embedded' => { 'osdi:events' => [event] } } }
+      let(:second_page_url) { 'http://site.com/marches?page=2' }
+
+      it 'fetches following pages if they exist' do
+        allow(Net::HTTP).to receive(:start).and_yield(http)
+
+        expect(Net::HTTP::Get).to receive(:new).with(URI(ActionNetworkGateway::ENDPOINT)).and_return(first_request)
+        expect(http).to receive(:request).with(first_request).and_return(double(body: first_response.to_json))
+
+        expect(Net::HTTP::Get).to receive(:new).with(URI(second_page_url)).and_return(second_request)
+        expect(http).to receive(:request).with(second_request).and_return(double(body: second_response.to_json))
+
+        expect(ActionNetworkGateway.marches).to eq([event, event])
+        expect(first_request).to have_key('OSDI-API-Token')
+        expect(second_request).to have_key('OSDI-API-Token')
+      end
     end
   end
 end
