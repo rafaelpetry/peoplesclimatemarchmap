edf5cc542cb3b0e69ba546327078fc8d6d6b8c92
ellaholmes <eholmes@thoughtworks.com>
2017-03-17 11:44:12 -0400

    EH - Moved external libraries to lib folder

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,6 @@
-//= require javascripts/jquery-3.1.1.min
-//= require javascripts/leaflet
-//= require javascripts/tabletop.min
+//= require javascripts/lib/jquery-3.1.1.min
+//= require javascripts/lib/leaflet
+//= require javascripts/lib/tabletop.min
 //= require javascripts/action_network_gateway
 //= require javascripts/google_sheets_gateway
 //= require javascripts/map
