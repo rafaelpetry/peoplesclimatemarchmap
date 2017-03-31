c332059bc388729315e4ae6c3a71f875fb41e1d8
Andrew McWilliams <andy@jahya.net>
2017-03-31 18:34:54 -0400

    Makes sure list under map stays up to date

--- assets/javascripts/map.js
+++ assets/javascripts/map.js
@@ -30,6 +30,7 @@ PCM.Map = (function() {
     PCM.Bus.locatePins(bounds);
     PCM.March.locatePins(bounds);
     PCM.Groups.locatePins(bounds);
+    setTimeout(locatePins, 1000);
   }
 
   function toggleLayer(toggleElement, markerGroup) {
@@ -42,10 +43,27 @@ PCM.Map = (function() {
 
   function listPinsWithinBounds(pin, bounds, tag){
     var latLng = pin.getLatLng();
-    if( bounds.contains(latLng) ){
+    var content = pin.getPopup().getContent();
+    if( bounds.contains(latLng) && !alreadyContains(tag, content)){
       $("#"+ tag +"_near_you ul").append('<li>'+pin.getPopup().getContent()+'</li>');
       $("#new_"+ tag + "_button").hide();
     }
+  }
+
+  function alreadyContains(tag, content) {
+    var found = false;
+    $("#"+ tag +"_near_you ul li").each(function(){
+      if(getHref($(this).html()) == getHref(content)){
+        found = true;
+      }
+    });
+    return found;
+  }
+
+  function getHref(str){
+    var contents = $.parseHTML('<div>' + str + '</div>');
+    contents = $(contents).find('a');
+    return contents.attr('href');
   }
 
   return {
