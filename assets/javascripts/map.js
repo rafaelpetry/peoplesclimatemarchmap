2667cd548a8a6ec231fbabbc656bd5d35cda4cdf
Andrew McWilliams <andy@jahya.net>
2017-04-04 13:07:04 -0400

    Re-implements 'Makes sure list under map stays up to date' but limits refreshes

--- assets/javascripts/map.js
+++ assets/javascripts/map.js
@@ -1,8 +1,7 @@
 PCM.Map = (function() {
-  var map;
+  var map, numRefreshes;
 
   function createMap(mapContainer) {
-
     map = L.map(mapContainer).setView([39.8282, -98.5795], 4);
     L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
       attribution: '<a href=https://wikimediafoundation.org/wiki/Terms_of_Use>Wikimedia</a> | &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -17,11 +16,11 @@ PCM.Map = (function() {
     PCM.Groups.groupMarkers.addTo(map);
 
     $('#climate_map_search').on('submit', PCM.Search.performSearch);
-
   }
 
   function setView(lat, lng) {
     map.setView([lat, lng], 11);
+    numRefreshes = 0;
     locatePins();
   }
 
@@ -30,6 +29,10 @@ PCM.Map = (function() {
     PCM.Bus.locatePins(bounds);
     PCM.March.locatePins(bounds);
     PCM.Groups.locatePins(bounds);
+    numRefreshes++;
+    if(numRefreshes < 5) {
+      setTimeout(locatePins, 1000);
+    }
   }
 
   function toggleLayer(toggleElement, markerGroup) {
@@ -42,10 +45,27 @@ PCM.Map = (function() {
 
   function listPinsWithinBounds(pin, bounds, tag){
     var latLng = pin.getLatLng();
-    if( bounds.contains(latLng) ){
+    var content = pin.getPopup().getContent();
+    if(bounds.contains(latLng) && !alreadyContains(tag, content)){
       $("#"+ tag +"_near_you ul").append('<li>'+pin.getPopup().getContent()+'</li>');
       $("#new_"+ tag + "_button").hide();
     }
+  }
+
+  function alreadyContains(tag, content) {
+    var found = false;
+    $("#"+ tag +"_near_you ul li").each(function(){
+      if(getHref($(this).html()) == getHref(content)){
+        found = true;
+      }
+    });
+    return found;
+  }
+
+  function getHref(str){
+    var contents = $.parseHTML('<div>' + str + '</div>');
+    contents = $(contents).find('a');
+    return contents.attr('href');
   }
 
   return {
